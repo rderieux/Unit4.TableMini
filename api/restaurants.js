db233441f4c707a407a9e13d70756476dda4680e
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-14 09:17:05 -0600

    Add endpoint method GET all restaurants, add endpoint method GET single restaurant with all reservations for that restaurant

--- api/restaurants.js
+++ api/restaurants.js
@@ -5,3 +5,23 @@ module.exports = router;
 const prisma = require("../prisma");
 
 // TODO: routes!
+router.get("/", async (req, res, next) => {
+  try {
+    const restaurants = await prisma.restaurant.findMany();
+    res.json(restaurants);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const restaurant = await prisma.reservation.findFirstOrThrow({
+      where: { id: +id },
+      include: { reservations: true },
+    });
+  } catch (error) {
+    next(error);
+  }
+});
--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,27 @@
 const prisma = require("../prisma");
 const seed = async () => {
   // TODO: Create 3 restaurants with 5 reservations each
+  // Loop through 3 restaurants
+  for (i = 0; i < 3; i++) {
+    const reservations = [];
+    // Loop through 5 reservations
+    for (ii = 0; ii < 5; ii++) {
+      reservations.push({
+        name: `Person ${i}${ii}`,
+        email: `${i}${ii}@email.com`,
+        partySize: Math.floor(Math.random() * 10) + 1,
+      });
+    }
+    // Create a single restaurant with nested reservations
+    await prisma.restaurant.create({
+      data: {
+        name: `Restaurant ${i + 1}`,
+        reservations: {
+          create: reservations,
+        },
+      },
+    });
+  }
 };
 seed()
   .then(async () => await prisma.$disconnect())
