11b80aec615f291343c25b0468e35e2ccbdfcbff
Robert DeRieux <ppldiecomplex@gmail.com>
2024-10-14 09:29:12 -0600

    Add POST method endpoint to create a new reservation

--- api/restaurants.js
+++ api/restaurants.js
@@ -17,11 +17,24 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const restaurant = await prisma.reservation.findUniqueOrThrow({
+    const restaurant = await prisma.restaurant.findUniqueOrThrow({
       where: { id: +id },
       include: { reservations: true },
     });
     res.json(restaurant);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.post("/:id/reservations", async (req, res, next) => {
+  const { id } = req.params;
+  const { name, email, partySize } = req.body;
+  try {
+    const reservation = await prisma.reservation.create({
+      data: { name, email, partySize: +partySize, restaurantId: +id },
+    });
+    res.status(201).json(reservation);
   } catch (error) {
     next(error);
   }
